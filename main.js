ffa366bbe2a6f19e413bd7fa69b204ab6492d911
briones-9r-ay2526 <briones.sa@obmontessori.edu.ph>
2026-03-26 17:48:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function checkGrade() {
+
+    let grade = document.getElementById("finalgrade").value;
+
+    if (Number(grade) >= 94 && grade <= 100) {
+        window.alert("Excellent!");
+    }
+    else if (Number(grade) >= 87 && grade <= 93) {
+        window.alert ("Standard!");
+    }
+    else if (Number(grade) >= 80 && grade <= 86) {
+        window.alert ("Needs Improvement!");
+    }
+    else {
+        window.alert ("Failed!");
+    }
+}
